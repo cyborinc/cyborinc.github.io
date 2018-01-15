501eb610ea05597babd9f69f05504e810f1df06b
Kris Howard <kris@mail.usf.edu>
2018-01-14 23:33:43 -0500

    Begin setting up the countdown - todo: figure out date

---
+++ js/countdown.js
@@ -0,0 +1,15 @@
+var countDownDate = new Date("Mar 1, 2018 00:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("timer").innerHTML = ("0" + days).slice(-2) + "  :  " + ("0" + hours).slice(-2) + "  :  "
+  + ("0" + minutes).slice(-2) + "  :  " + ("0" + seconds).slice(-2);
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "Welcome.";
+  }
+}, 1000);
